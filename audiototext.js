94332a39071e200aad59ed4d561f012cd1222e76
ps6462843@gmail.com <ps6462843@gmail.com>
2026-05-20 19:47:58 +0530

    Added Groq audio transcription feature

--- audiototext.js
+++ audiototext.js
@@ -1,21 +1,41 @@
 import openAI from "openai";
 import dotenv from 'dotenv'
 dotenv.config();
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
 import fs from 'fs';
 const client = new openAI({
     apiKey:process.env.GROQ_API_Key,
     baseURL: "https://api.groq.com/openai/v1",
 });
 
-async function main(){
-    const textResponse = await client.audio.transcriptions.create({
-        file: fs.createReadStream("Ghost - Mary On A Cross (Official Music Video).mp3"),
+const app = express();
+app.get("/", (req, res) => {
+    res.send(`<form action="/upload" method="post" enctype="multipart/form-data"> 
+        <input type="file" name="audiofile" />
+        <button type="submit">Upload</button>
+    </form>`);
+})
+const storage = multer.diskStorage({
+      destination: 'uploads/',
+    filename:(req, file,cb)=>{
+        const ext = path.extname(file.originalname);
+        cb(null,Date.now()+ext);
+    }
+});
+const upload = multer({storage});
+app.post("/upload", upload.single("audiofile"), async (req, res) => {
+    const filepath = req.file.path;
+   const textResponse = await client.audio.transcriptions.create({
+        file: fs.createReadStream(filepath),
         model: "whisper-large-v3-turbo",
         language: "en",
     });
+      fs.writeFileSync("transcription.txt", textResponse.text, "utf-8");
+      res.send(textResponse.text);
+});
 
-    console.log(textResponse.text);
-    const rawText = textResponse.text;
-    fs.writeFileSync("transcription.txt", rawText, "utf-8");
-}
-main();
+app.listen(3200);
+
+
