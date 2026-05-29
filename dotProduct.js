9ce553024205fe2621813272f96a2cf240b3741e
ps6462843@gmail.com <ps6462843@gmail.com>
2026-05-29 08:38:51 +0530

    fix error dotProduct.js

--- dotProduct.js
+++ dotProduct.js
@@ -6,13 +6,20 @@ import { generateEmbedding } from './embjson.js';
 
 let  embeddingData = fs.readFileSync('embeddings.json');
 embeddingData = JSON.parse(embeddingData);
-// console.log(embeddingData);
-
+// console.log(embeddingData[0]);
+// console.log(embeddingData[0]);
+// console.log(typeof embeddingData[0]);
 let animal = await generateEmbedding("animal");
+animal = animal[0];
 // console.log(animal[0]);
- 
+//  console.log("embedding length:", embeddingData[0].embedding.length);
+// console.log("animal length:", animal.length);
+// console.log(typeof animal[0]);
+// console.log(animal[0]);
 const similarity = embeddingData.map((embeddingItem)=>{
+    
     const embedding = embeddingItem.embedding;
+    // console.log(embedding);
     return embedding.map((item,index)=>{
         return embedding[index] * animal[index];
     }).reduce((a,b)=>a+b,0);
